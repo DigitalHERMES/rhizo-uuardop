6bc22d7d5a23144f74a2bb162dc909ffad89a102
ratitu <rogergodoy@gmail.com>
2019-10-06 18:56:00 -0300

    v0

---
+++ gui/list.php
@@ -0,0 +1,16 @@
+<?php
+$dir = "uploads/";
+
+// Open a directory, and read its contents
+
+if (is_dir($dir)){
+  if ($dh = opendir($dir)){
+    while (($file = readdir($dh)) !== false){
+        if ($file == '.' || $file == '..') {
+        continue;}
+      echo "Arquivo:" . $file . "</br>";
+    }
+    closedir($dh);
+  }
+}
+?>
--- gui/upload.php
+++ gui/upload.php
@@ -1,58 +1,62 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta charset="UTF-8">
+    <h1>  <center>rhizo-uuardop</center></h1>
+    <title>Serviços de Comunicação Digital</title>
+    <link rel="stylesheet" href="style.css">
+	</head>
+
+<body>
+
+<div class="w3-container w3-teal">
+    <h2>rhizo uma gravação</h2>
+</div>
 <?php
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadPic = 0;
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadPic = 1;
-        $uploadOk = 1;
-    } else {
-//        echo "File is not an image. Proceding normally";
-        $uploadOk = 1;
-        $uploadPic = 0;
-    }
-}
+
 // Check if file already exists
 if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
+    echo "Esse arquivo já esta na fila.";
     $uploadOk = 0;
 }
-
-
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" && $uploadPic == 1) {
-    echo "We have a common JPG, JPEG, PNG or GIF!";
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 50000000) {
+    echo "O arquivo é muito grande.";
+    $uploadOk = 0;
 }
-
-// Check file size and if it is picture, reduce the size...
-// limit not to reduce size is 50k!
-if (($_FILES["fileToUpload"]["size"] > 50*1024) && $uploadPic == 1 ) { // 10MB max
-    echo "Sua imagem é muito grande - convertendo para um tamanho menor.<br />";
-    $command = "ffmpeg -i " . ;
-    ob_start();
-    system($command , $return_var);
-    $output = ob_get_contents();
-    ob_end_clean();
-    echo "Output: " . $output . " Return value: " . $return_var; 
-    $uploadOk = 1;
-}
-
-
+// Allow certain file formats
+//if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+//&& $imageFileType != "gif" )  {
+//    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+//    $uploadOk = 0;
+//}
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) { //  should I run UUCP from here?
-    // uucp -C buffer.c central\!\~/
-        echo "O arquivo ". basename( $_FILES["fileToUpload"]["name"]). " foi adicionado com sucesso.";
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "</br>O arquivo  ". basename( $_FILES["fileToUpload"]["name"]). " esta na fila.</br>";
+        //$list= scandir($target_dir,1);
+        //print_r($list);
+        include 'list.php';
     } else {
-        echo "Erro na cópia do arquivo....";
+        echo "Sorry, there was an error uploading your file.";
     }
 }
 ?>
+    
+    <form action="php3.php" method="post" enctype="multipart/form-data">
+        <p>
+        Carregar mais arquivos?
+        
+     </br><input type="file" name="fileToUpload" id="fileToUpload"></br>
+      <input type="submit" value="Colocar arquivo na fila" name="submit">
+      </br><button class="button" type="submit" value="queue"> Enviar fila</button>
+      </p>
+</body>
+</html>
