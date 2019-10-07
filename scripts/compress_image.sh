5b7c66369628f92c40e0f283f523762881cf4391
Rafael Diniz <rafael@riseup.net>
2019-10-07 09:54:52 -0300

    compress_image.sh script seems in good shape.

--- scripts/compress_image.sh
+++ scripts/compress_image.sh
@@ -1,7 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 # uso:
 # compress_image.sh arquivo_para_comprimir.jpg
-TEMPFILE=/tmp/temp.jpg
+MAX_SIZE=${MAX_SIZE:=51200} # 50kB file size limit
+QUALITY=75 # initial start quality to try...
 
-convert -resize "840x840>" "${1}" pnm:- | /opt/mozjpeg/bin/cjpeg -tune-ms-ssim -quality 70 > ${TEMPFILE}
+TEMPFILE=/tmp/temp-$$.jpg
+
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 image_filename.jpg"
+  exit 1
+fi
+
+# while [[ stat -c ${1} ]]
+# echo $(stat -c%s "${1}")
+cp -f "${1}" ${TEMPFILE}
+
+while [ "$(stat -c%s "${TEMPFILE}")" -gt "$MAX_SIZE" ] && [ "$QUALITY" -gt "5" ]; do
+#  echo $(stat -c%s "${TEMPFILE}")
+  convert -resize "840x840>" "${1}" pnm:- | /opt/mozjpeg/bin/cjpeg -tune-ms-ssim -quality ${QUALITY} > ${TEMPFILE}
+  QUALITY=$((QUALITY-10))
+#  echo ${QUALITY}
+done;
+
 mv ${TEMPFILE} "${1}"
