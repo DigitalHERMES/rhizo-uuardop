c1cea2916b0e53f8f5efe5706b8de9dd4adf547d
Rafael Diniz <rafael@riseup.net>
2019-10-07 23:59:04 -0300

    added view_log php

---
+++ gui/view_log.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta charset='UTF-8'/>
+    <title>Serviços de Comunicação Digital</title>
+    <link rel='stylesheet' href='styles.css'/>
+  </head>
+  <body>
+    <h2>Estação: <?php include('get_name.php') ?></h2>
+<br />
+    <h2>Log do Sistema:</h2>
+<br />
+<?php 
+    $command = "uulog";
+    ob_start();
+    system($command , $return_var);
+    $output = ob_get_contents();
+    ob_end_clean();
+    echo $output; 
+?>
+<br/>
+<br/>
+
+</body>
+</html>
