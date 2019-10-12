85ebc258af323267f42b96fd7398ccf655983b7f
ratitu <rogergodoy@gmail.com>
2019-10-12 17:10:36 -0300

    index.html is the landing page, index.php is the upload page (and other things)

--- gui/index.php
+++ gui/index.php
@@ -1,43 +1,72 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-  <head>
-    <meta charset='UTF-8'/>
+<head>
+    <meta charset="UTF-8"</meta>
+    <h1>  <center>rhizo-uuardop</center></h1>
     <title>Serviços de Comunicação Digital</title>
-<!--    <link rel='stylesheet' href='styles.css'/> -->
-  </head>
-  <body>
-    <center>
-      
-      <h2>Estação: <?php include('get_name.php') ?></h2>
-      <h5>Estado do Sistema: <?php include 'running.php'; ?></h5>
-      <hr />
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">    
+</head>
 
-      <p>
-	<h1><a href="load.php">Carregar Arquivo para Transmissão</a></h1>
-      </p>
+<body>
+    
+   
+    
+    <div class='header'>
+        <h2>Estação: AA2AAA</h2>
+    <h6>Estado do Sistema: 
+        <?php include 'running.php'; ?>
+        
+    </h6></div>
 
-      <p>
-	<h1><a href="tx.php">Transmitir</a></h1>
-      </p>
 
-      <p>
-	<h1><a href="list_job.php">Listar Arquivos Carregados para Transmissão</a></h1>
-      </p>
+    <h2>Envio de arquivo</h2>
+    <form action="php3.php" method="post" enctype="multipart/form-data">
+      Escolha o arquivo:<i class="material-icons">
+folder_open
+</i> 
+      <input type="file" name="fileToUpload" id="fileToUpload">
+      <input type="submit" value="Colocar arquivo na fila" name="submit"><i class="material-icons">
+cloud_upload
+</i><br/>
+    </form>
+            
+    
+    
 
-      <p>
-	<h1><a href="arquivos/">Recebimento de arquivos</a></h1>
-      </p>
-      <p>
-	<h1><a href="kill_job.php">Cancelar transmissão</a></h1>
-      </p>
-      <p>
-	<h1><a href="list_job.php">Fila de transmissão</a></h1>
-      </p>
-      <p>
-	<h1><a href="view_log.php">Ver log</a></h1>
-      </p>
-    </center>
-    <br/>
-    <br/>
-  </body>
+   
+<br/>
+
+<div class='results'>
+<br/>
+Arquivos na fila para serem enviados.
+</br>
+</br>
+
+    <?php include 'list.php'; ?>
+</div>
+     <div class="body">
+    Escolher destinatario
+    <select class="form" id="prefix" name="prefix">
+    <option>Selecionar </option>
+    <option>111111</option>
+    <option>111111</option>
+    <option>111111</option>
+    <option>111111</option>
+    <option>111111</option>
+    <option>111111</option>
+    <option>111111</option>
+    <option>111111</option>
+    </select>
+    <form action="sendQueue.php" method="post" enctype="multipart/form-data">
+     Enviar todos os arquivos que estão na fila  <input type="submit" value="Enviar todos os arquivos da fila" name="submit"><br/>
+    </form></div>
+<form action="index.html" method="post" enctype="multipart/form-data">
+  <input type="submit" value="Voltar a pagina inicial" name="index"><br/>
+    </form>
+
+    
+            
+
+</body>
 </html>
---
+++ gui/index_nocss.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta charset='UTF-8'/>
+    <title>Serviços de Comunicação Digital</title>
+<!--    <link rel='stylesheet' href='styles.css'/> -->
+  </head>
+  <body>
+    <center>
+      
+      <h2>Estação: <?php include('get_name.php') ?></h2>
+      <h5>Estado do Sistema: <?php include 'running.php'; ?></h5>
+      <hr />
+
+      <p>
+	<h1><a href="load.php">Carregar Arquivo para Transmissão</a></h1>
+      </p>
+
+      <p>
+	<h1><a href="tx.php">Transmitir</a></h1>
+      </p>
+
+      <p>
+	<h1><a href="list_job.php">Listar Arquivos Carregados para Transmissão</a></h1>
+      </p>
+
+      <p>
+	<h1><a href="arquivos/">Recebimento de arquivos</a></h1>
+      </p>
+      <p>
+	<h1><a href="kill_job.php">Cancelar transmissão</a></h1>
+      </p>
+      <p>
+	<h1><a href="list_job.php">Fila de transmissão</a></h1>
+      </p>
+      <p>
+	<h1><a href="view_log.php">Ver log</a></h1>
+      </p>
+    </center>
+    <br/>
+    <br/>
+  </body>
+</html>
