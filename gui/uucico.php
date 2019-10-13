ca884bc5620c8980bf7449831e50ef0d31b3ae4c
Rafael Diniz <rafael@riseup.net>
2019-10-13 13:44:01 -0300

    calling uucico with nohup

--- gui/uucico.php
+++ gui/uucico.php
@@ -15,7 +15,7 @@
 // fix me - inverted logic...
 if (!isset($_POST['sendall']))
 {
-    $command = "sudo uucico  --master -f";
+    $command = "nohup sudo uucico  --master -f > /dev/null";
     echo "UUCP Command: " . $command . "<br />";
     ob_start();
     system($command , $return_var);
@@ -29,7 +29,7 @@ if (!isset($_POST['sendall']))
         echo "ERRO: Estação de origem é igual estação de destino! <br />";
     } else 
     {
-        $command = "sudo uucico -S " . $_POST['prefix'] . "";
+        $command = "nohup sudo uucico -S " . $_POST['prefix'] . " > /dev/null";
         echo "UUCP Command: " . $command . "<br />";
         ob_start();
         system($command , $return_var);
