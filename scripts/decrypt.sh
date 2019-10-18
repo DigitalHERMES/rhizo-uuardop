d0329722e351cb1f948e30347e891ceb569bca29
Rafael Diniz <rafael@riseup.net>
2019-10-17 21:45:50 -0300

    fix for decrypt.sh

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -8,10 +8,6 @@ if [ $# -lt 2 ]; then
   exit 1
 fi
 
-dec_file=$(basename ${1} .gpg)
+dec_file=$(dirname "${1}")/$(basename ${1} .gpg)
 
 gpg -o "${dec_file}" -d --batch --passphrase "${2}" --yes "${1}"
-
-
-# rm "${1}"
-# gpg -o eu.png -d --batch --passphrase 1234 eu.png.gpg
