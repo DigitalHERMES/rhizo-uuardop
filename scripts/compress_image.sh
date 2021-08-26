29c10394db90377d9d606dcf2fc09ac5c0ec3db6
Rafael Diniz <rafael@riseup.net>
2021-08-27 01:29:18 +0300

    added vvcenc code

--- scripts/compress_image.sh
+++ scripts/compress_image.sh
@@ -4,6 +4,8 @@
 MAX_SIZE=${MAX_SIZE:=51200} # 50kB file size limit
 QUALITY=75 # initial start quality to try...
 
+VVCENC=/usr/bin/vvcencapp
+
 # vvc and evc  are the state of the art, no integration to userlad
 # avif and heic are already implemented and integrated to userland
 # jpg is the legacy format
@@ -23,6 +25,7 @@ IMAGE_FORMAT="${output_file##*.}"
 
 
 TEMPFILE=/tmp/temp-$$.${IMAGE_FORMAT}
+TEMPFILEYUV=/tmp/temp-$$.yuv
 
 echo "Original file size = $(stat -c%s "${input_file}")"
 
@@ -31,22 +34,28 @@ echo "Original file size = $(stat -c%s "${input_file}")"
 cp -f "${input_file}" ${TEMPFILE}
 
 if [ ${IMAGE_FORMAT} = "evc" ]; then
-
+ 
   while [ "$(stat -c%s "${TEMPFILE}")" -gt "$MAX_SIZE" ] && [ "$QUALITY" -gt "5" ]; do
     # convert -resize "840x840>" "${input_file}" pnm:- | /opt/mozjpeg/bin/cjpeg -quality ${QUALITY} > ${TEMPFILE}
     echo "TODO: EVC - convert to yuv then encode"
     QUALITY=$((QUALITY-10))
   done;
+  
+elif [ ${IMAGE_FORMAT} = "vvc" ]; then
 
-elif
-
-if [ ${IMAGE_FORMAT} = "vvc" ]; then
-
-  while [ "$(stat -c%s "${TEMPFILE}")" -gt "$MAX_SIZE" ] && [ "$QUALITY" -gt "5" ]; do
+#  while [ "$(stat -c%s "${TEMPFILE}")" -gt "$MAX_SIZE" ] && [ "$QUALITY" -gt "5" ]; do
     # convert -resize "840x840>" "${input_file}" pnm:- | /opt/mozjpeg/bin/cjpeg -quality ${QUALITY} > ${TEMPFILE}
+    # use -debug all to get the size!
+    resolution=$(convert-im6 -debug all -resize "840x840>" ${input_file} -sampling-factor 4:2:0 -depth 8 -colorspace Rec709YCbCr ${TEMPFILEYUV} 2>&1 | grep -i Heap|  sed -n 2p |  rev | cut -f2 -d " " | rev)
+    echo $resolution
+    $(VVCENC) -i ${TEMPFILEYUV} -s $resolution -c yuv420 -b 10000 -r 1 -o  ${TEMPFILE}
+    exit 0
+    #
+    #    ffmpeg -i DRONE_DJI_0008.JPG -c:v rawvideo -pixel_format yuv420p -vf scale=-1:840  output_720x480p.yuv
+    
     echo "TODO: VVC - convert to yuv then encode"
     QUALITY=$((QUALITY-10))
-  done;
+ # done;
 
 elif [ ${IMAGE_FORMAT} = "avif" ]; then
 
