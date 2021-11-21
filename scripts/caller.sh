6a0dace06e21f6aeb405b3960615c85c6f3ef0c5
Rafael Diniz <rafael@riseup.net>
2021-11-21 21:50:02 +0300

    caller should work now, given a time range and the list of stations to call

--- scripts/caller.sh
+++ scripts/caller.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
 
-DELAY=2
+start_time_hour=9
+start_time_minute=30
 
-HOSTS=(ac1 ac2 ac3 ac4 ac5 ac6 ac7)
+end_time_hour=20
+end_time_minute=30
 
-for t in ${HOSTS[@]}; do
-          echo uucico -r1 $t
-          #uucico -r1 $t
-          sleep $DELAY
+# 30sec between each call
+DELAY=30
+
+HOSTS="ac1 ac3 ac4 ac5 ac6 ac7 ac9"
+
+current_hour=$(date +%H)
+current_minute=$(date +%M)
+
+while [[ ${current_hour} -eq ${start_time_hour} &&
+           ${current_minute} -ge ${start_time_minute} ]] ||
+        [[ ${current_hour} -gt ${start_time_hour} &&
+             ${current_hour} -lt ${end_time_hour} ]] ||
+              [[ ${current_hour} -eq ${end_time_hour} &&
+                   ${current_minute} -le ${end_time_minute} ]]
+
+do
+
+  echo "current time ${current_hour}h ${current_minute}min"
+  echo "start time ${start_time_hour}h ${start_time_minute}min"
+  echo "end time ${end_time_hour}h ${end_time_minute}min"
+  echo
+
+  for t in ${HOSTS}; do
+    echo "Calling ${t}"
+    uucico -S ${t}
+    sleep ${DELAY}
+  done
+
+  current_hour=$(date +%H)
+  current_minute=$(date +%M)
+
 done
