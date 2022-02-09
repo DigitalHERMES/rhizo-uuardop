d8a78e7a1bea12975407479861c6c5b705d51892
Carlos Paulino <chgp@riseup.net>
2022-02-09 19:07:02 -0300

    caller.sh integration with station-api endpoint caller

--- scripts/caller.sh
+++ scripts/caller.sh
@@ -1,40 +1,52 @@
 #!/bin/bash
 
-start_time_hour=9
-start_time_minute=30
-
-end_time_hour=20
-end_time_minute=30
 
 # 30sec between each call
-DELAY=30
+DELAY=1
 
-HOSTS="ac1 ac3 ac4 ac5 ac6 ac7 ac9"
-
-current_hour=$(date +%H)
-current_minute=$(date +%M)
-
-while [[ ${current_hour} -eq ${start_time_hour} &&
-           ${current_minute} -ge ${start_time_minute} ]] ||
-        [[ ${current_hour} -gt ${start_time_hour} &&
-             ${current_hour} -lt ${end_time_hour} ]] ||
-              [[ ${current_hour} -eq ${end_time_hour} &&
-                   ${current_minute} -le ${end_time_minute} ]]
-
+while true
 do
+    hosts=($(curl -s http://localhost/api/caller/ | jq --raw-output '.[0] | .stations[] | @sh' ))
+    timers_start=($(curl -s http://localhost/api/caller | jq --raw-output '.[] | .starttime ' ))
+    timers_stop=($(curl -s http://localhost/api/caller | jq --raw-output '.[] | .stoptime ' ))
+    current_hour=$(date +%H)
+    current_minute=$(date +%M)
 
-  echo "current time ${current_hour}h ${current_minute}min"
-  echo "start time ${start_time_hour}h ${start_time_minute}min"
-  echo "end time ${end_time_hour}h ${end_time_minute}min"
-  echo
+    for (( c=0; c<${#timers_start[@]}; c++ )); do
 
-  for t in ${HOSTS}; do
-    echo "Calling ${t}"
-    uucico -S ${t}
+	      start_time_hour=$(echo ${timers_start[c]} | cut -d ':' -f 1)
+  	    start_time_minute=$(echo ${timers_start[c]} | cut -d ':' -f 2)
+	      end_time_hour=$(echo ${timers_stop[c]} | cut -d ':' -f 1)
+	      end_time_minute=$(echo ${timers_stop[c]} | cut -d ':' -f 2)
+
+	      echo "DBG: schedule " $c
+	      echo "current time ${current_hour}h ${current_minute}min"
+	      echo "start time ${start_time_hour}h ${start_time_minute}min"
+	      echo "end time ${end_time_hour}h ${end_time_minute}min"
+
+	      if [[ ${current_hour} -eq ${start_time_hour} &&
+		              ${current_minute} -ge ${start_time_minute} ]] ||
+               [[ ${current_hour} -gt ${start_time_hour} &&
+		                  ${current_hour} -lt ${end_time_hour} ]] ||
+               [[ ${current_hour} -eq ${end_time_hour} &&
+                      ${current_minute} -le ${end_time_minute} ]]
+
+	      then
+
+	          for t in ${hosts[*]}; do
+		            echo "Calling ${t}"
+		            # uucico -S ${t}
+		            sleep ${DELAY}
+	          done
+
+	          current_hour=$(date +%H)
+	          current_minute=$(date +%M)
+	      else
+	          echo "will not run"
+	          echo
+	      fi
+    done
+
     sleep ${DELAY}
-  done
-
-  current_hour=$(date +%H)
-  current_minute=$(date +%M)
 
 done
